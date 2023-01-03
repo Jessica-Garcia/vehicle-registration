c22ed537c92bc3514eb000d6a296b5ab23b09540
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-01-03 12:30:26 -0300

    refactor: configure themes

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  white: '#fff',
+  black: '#000000',
+
+  'purple-100': '#C59EDB',
+  'purple-200': '#A858D3',
+  'purple-300': '#8043A1',
+  'purple-400': '#432354',
+  'purple-500': '#4C3D54',
+}
