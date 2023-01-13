d9a6efe0b7f4591ed8d7c89d6bed581b68fa00a2
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-01-13 19:28:44 -0300

    refactor: change global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,12 +9,17 @@ export const GlobalStyle = createGlobalStyle`
 
    :focus{
         outline: 0;
-        box-shadow: 0 0 0 2px ${(props) => props.theme["purple-500"]};
+        box-shadow: 0 0 0 2px ${(props) => props.theme["purple-100"]};
    }
 
    body {
-        background: ${(props) => props.theme["purple-900"]};
+        background: linear-gradient(to right bottom, 
+          ${(props) => props.theme["blue-200"]}, 
+          ${(props) => props.theme["purple-300"]},
+          ${(props) => props.theme["purple-100"]}
+          );
         color: ${(props) => props.theme["gray-100"]};
+        
    }
 
    body, input, textarea, button {
