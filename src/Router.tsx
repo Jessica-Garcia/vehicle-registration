53171c71f171ee71180d3a8a7eb9228925815cd1
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-01-24 16:13:09 -0300

    refactor: create, view and edit on the same page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Vehicle } from "./pages/Vehicle";
 import { DefaultLayout } from "./layouts/DefaultLayout";
-import { AddVehicle } from "./pages/AddVehicle";
 
 export const Router = () => {
   return (
@@ -10,7 +9,8 @@ export const Router = () => {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/vehicle/:id" element={<Vehicle />} />
-        <Route path="/addVehicle" element={<AddVehicle />} />
+        <Route path="/vehicle/add" element={<Vehicle />} />
+        <Route path="vehicle/edit/:id" element={<Vehicle />} />
       </Route>
     </Routes>
   );
--- src/pages/Vehicle/index.tsx
+++ src/pages/Vehicle/index.tsx
@@ -1,13 +1,49 @@
-import { ArrowFatLeft, Pencil, Trash } from "phosphor-react";
-import { ButtonsContainer, VehicleInfo } from "./styles";
+import { ArrowFatLeft } from "phosphor-react";
+import { VehicleInfo } from "./styles";
 import { IVehicle } from "../../interfaces/IVehicle";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 export const Vehicle = () => {
-  const [vehicle, setVehicle] = useState<IVehicle>();
+  const [vehicle, setVehicle] = useState<IVehicle>({} as IVehicle);
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVehicle((currentValue) => ({
+      ...currentValue,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const editVehicle = async () => {
+    await axios.put<IVehicle>(`http://localhost:3000/vehicles/${id}`, vehicle);
+  };
+
+  const addVehicle = async () => {
+    await axios.post<IVehicle>("http://localhost:3000/vehicles", {
+      id: uuidv4(),
+      ...vehicle,
+    });
+  };
+
+  const saveVehicle = () => {
+    if (vehicle.id) {
+      editVehicle();
+    } else {
+      addVehicle();
+    }
+  };
+
+  const handleSaveVehicle = () => {
+    try {
+      saveVehicle();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     const getVehicle = async () => {
@@ -26,41 +62,63 @@ export const Vehicle = () => {
 
   return (
     <VehicleInfo>
-      {vehicle ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Placa</th>
-              <th>Marca</th>
-              <th>Modelo</th>
-              <th>Versão</th>
-              <th>Ano</th>
-              <th>Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{vehicle.licensePlate}</td>
-              <td>{vehicle.brand}</td>
-              <td>{vehicle.model}</td>
-              <td>{vehicle.version}</td>
-              <td>{vehicle.year}</td>
-              <td>
-                <ButtonsContainer>
-                  <NavLink to="/">
-                    <ArrowFatLeft weight="bold" />
-                  </NavLink>
-                  <NavLink to="/">
-                    <Pencil weight="bold" />
-                  </NavLink>
-                  <NavLink to="/">
-                    <Trash weight="bold" />
-                  </NavLink>
-                </ButtonsContainer>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      {vehicle.id ? (
+        <form
+          onSubmit={() => {
+            handleSaveVehicle();
+            navigate("/");
+          }}
+        >
+          <label htmlFor="licensePlate">Placa</label>
+          <input
+            type="text"
+            name="licensePlate"
+            id="licensePlate"
+            value={vehicle?.licensePlate}
+            onChange={handleInputChange}
+            required
+          />
+          <label htmlFor="brand">Marca</label>
+          <input
+            type="text"
+            name="brand"
+            id="brand"
+            value={vehicle?.brand}
+            onChange={handleInputChange}
+            required
+          />
+          <label htmlFor="model">Modelo</label>
+          <input
+            type="text"
+            name="model"
+            id="model"
+            value={vehicle?.model}
+            onChange={handleInputChange}
+            required
+          />
+          <label htmlFor="year">Ano</label>
+          <input
+            type="text"
+            name="year"
+            id="year"
+            value={vehicle?.year}
+            onChange={handleInputChange}
+            required
+          />
+          <label htmlFor="version">Versão</label>
+          <input
+            type="text"
+            name="version"
+            id="version"
+            value={vehicle?.version}
+            onChange={handleInputChange}
+            required
+          />
+          <NavLink to="/">
+            <ArrowFatLeft /> voltar
+          </NavLink>
+          <button>Salvar</button>
+        </form>
       ) : (
         <div>
           <h2>Veículo não encontrado</h2>
